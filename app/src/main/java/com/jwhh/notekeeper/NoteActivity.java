106e420690d6ac8017f9f6c3a99b46b6b475a60e
Maksym Rybak <rybak.maksym@gmail.com>
2020-09-09 17:25:08 +0200

    semplify UI, add cancel menu to return back to notes list without saving changes to opened note

--- app/src/main/java/com/jwhh/notekeeper/NoteActivity.java
+++ app/src/main/java/com/jwhh/notekeeper/NoteActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -12,10 +14,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.jwhh.notekeeper.model.CourseInfo;
+import com.jwhh.notekeeper.model.DataManager;
+import com.jwhh.notekeeper.model.NoteInfo;
+
+import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
     public static final String NOTE_POSITION = "com.jwhh.notekeeper.NOTE_POSITION";
     public static final int POSITION_NOT_SET = -1;
+
+    private Spinner spinnerCourses;
+    private EditText textNoteTitle;
+    private EditText textNoteText;
+
+    private int notePosition;
+    private NoteInfo note;
+    private boolean isNewNote;
+
+    private boolean isCanceling;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +39,65 @@ public class NoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        readDisplayStateValues();
+
+        spinnerCourses = findViewById(R.id.spinner_courses);
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCourses.setAdapter(adapterCourses);
+
+        textNoteTitle = findViewById(R.id.text_note_title);
+        textNoteText = findViewById(R.id.text_note_text);
+
+        if (!isNewNote) {
+            displayNote(spinnerCourses, textNoteTitle, textNoteText);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (isCanceling) {
+            if (isNewNote)
+                DataManager.getInstance().removeNote(notePosition);
+        } else {
+            saveNote();
+        }
+    }
+
+    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        int courceIndex = courses.indexOf(note.getCourse());
+
+        spinnerCourses.setSelection(courceIndex);
+        textNoteTitle.setText(note.getTitle());
+        textNoteText.setText(note.getText());
+    }
+
+    private void readDisplayStateValues() {
+        Intent intent = getIntent();
+//        note = intent.getParcelableExtra(NoteActivity.NOTE_POSITION);
+        int position = intent.getIntExtra(NoteActivity.NOTE_POSITION, NoteActivity.POSITION_NOT_SET);
+        isNewNote = position == NoteActivity.POSITION_NOT_SET;
+        if (isNewNote) {
+            createNewNote();
+        } else {
+            note = DataManager.getInstance().getNotes().get(position);
+        }
+    }
+
+    private void saveNote() {
+        note.setCourse((CourseInfo)spinnerCourses.getSelectedItem());
+        note.setTitle(textNoteTitle.getText().toString());
+        note.setText(textNoteText.getText().toString());
+    }
+
+    private void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        notePosition = dm.createNewNote();
+        note = dm.getNotes().get(notePosition);
     }
 
     @Override
@@ -43,16 +118,15 @@ public class NoteActivity extends AppCompatActivity {
         if (id == R.id.action_send_mail) {
             sendEmail();
             return true;
+        } else if (id == R.id.action_cancel) {
+            isCanceling = true;
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
     private void sendEmail() {
-        Spinner spinnerCourses = findViewById(R.id.spinner_courses);
-        TextView textNoteTitle = findViewById(R.id.text_note_title);
-        TextView textNoteText = findViewById(R.id.text_note_text);
-
         CourseInfo course = (CourseInfo) spinnerCourses.getSelectedItem();
 
         String subject = textNoteTitle.getText().toString();
