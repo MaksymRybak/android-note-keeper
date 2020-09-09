827325044e909a41a38fa73a16b5eaa0e923d03c
Maksym Rybak <maksym.rybak@zucchetti.it>
2020-09-09 10:20:05 +0200

    add activity for the list of notes, switched main/launcher activity to this one

---
+++ app/src/main/java/com/jwhh/notekeeper/NoteListActivity.java
@@ -0,0 +1,43 @@
+package com.jwhh.notekeeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.jwhh.notekeeper.model.DataManager;
+import com.jwhh.notekeeper.model.NoteInfo;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+        ListView listNotes = findViewById(R.id.list_notes);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+
+        listNotes.setAdapter(adapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
