6a76afcd5b654eee8b20c981a0bbb98a11fee1a3
Maksym Rybak <rybak.maksym@gmail.com>
2020-09-11 17:10:04 +0200

    example of interaction test

---
+++ app/src/androidTest/java/com/jwhh/notekeeper/NoteCreationTest.java
@@ -0,0 +1,31 @@
+package com.jwhh.notekeeper;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+
+@RunWith(AndroidJUnit4.class)
+public class NoteCreationTest {
+    @Rule
+    public ActivityTestRule<NoteListActivity> noteListActivityRule = new ActivityTestRule<>(NoteListActivity.class);
+
+    @Test
+    public void createNewNote() {
+//        ViewInteraction fabNewNote = onView(withId(R.id.fab));
+//        fabNewNote.perform(click());
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.text_note_title)).perform(typeText("Test note title"));
+        onView(withId(R.id.text_note_text)).perform(typeText("Test note text"),
+                closeSoftKeyboard());
+    }
+}
