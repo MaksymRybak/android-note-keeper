804fca5162da05ca5043a489f4c84e1e5e24e197
Maksym Rybak <rybak.maksym@gmail.com>
2020-09-11 17:51:55 +0200

    view assertion

--- app/src/androidTest/java/com/jwhh/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/jwhh/notekeeper/NoteCreationTest.java
@@ -4,28 +4,64 @@ import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import com.jwhh.notekeeper.model.CourseInfo;
+import com.jwhh.notekeeper.model.DataManager;
+import com.jwhh.notekeeper.model.NoteInfo;
+
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static org.junit.Assert.*;
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.hamcrest.Matchers.*;
+import static androidx.test.espresso.Espresso.pressBack;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+    static DataManager dataManager;
+
+    @BeforeClass
+    public static void classSetUp() {
+        dataManager = DataManager.getInstance();
+    }
+
     @Rule
     public ActivityTestRule<NoteListActivity> noteListActivityRule = new ActivityTestRule<>(NoteListActivity.class);
 
     @Test
     public void createNewNote() {
+        final CourseInfo courseInfo = dataManager.getCourse("java_lang");
+        final String noteTitle = "Test note title";
+        final String noteText = "Test note text";
+
 //        ViewInteraction fabNewNote = onView(withId(R.id.fab));
 //        fabNewNote.perform(click());
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("Test note title"));
-        onView(withId(R.id.text_note_text)).perform(typeText("Test note text"),
+
+        onView(withId(R.id.spinner_courses)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(courseInfo))).perform(click());
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(containsString(courseInfo.getTitle()))));
+
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle))
+            .check(matches(withText(containsString(noteTitle))));
+
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText),
                 closeSoftKeyboard());
+        // just as example
+        onView(withId(R.id.text_note_text)).check(matches(withText(containsString(noteText))));
+
+        pressBack();
+
+        int noteIndex = dataManager.getNotes().size() - 1;
+        NoteInfo createdNote = dataManager.getNotes().get(noteIndex);
+        assertEquals(courseInfo, createdNote.getCourse());
+        assertEquals(noteTitle, createdNote.getTitle());
+        assertEquals(noteText, createdNote.getText());
     }
 }
